b0151040aae7ac4f9b426832c23e43565a11e2e4
AmeyaBhalgat <79449162+AmeyaBhalgat@users.noreply.github.com>
2026-03-01 21:01:06 -0800

    Change author name in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
+    author= 'Ameya Bhalgat',
     author_email= 'abc@gmail.com',
     packages= find_packages(),
     install_requires = []
